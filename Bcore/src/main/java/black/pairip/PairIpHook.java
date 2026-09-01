ff7e73f6be357223f2766e21891942a932b911dd
hatdogg33 <hatdogg33@users.noreply.github.com>
2026-09-01 05:41:03 +0000

    Fix build: add missing java.util.Map import for hookServiceManager

--- Bcore/src/main/java/black/pairip/PairIpHook.java
+++ Bcore/src/main/java/black/pairip/PairIpHook.java
@@ -14,6 +14,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
